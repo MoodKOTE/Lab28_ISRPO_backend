e465d1531d0956f8fdb6a3ecfa41b97d37ce0938
Лепилкин Максим <maksimlepilkin7@gmail.com>
2026-04-27 19:49:43 +0300

    Step 5: GamesController with GET, POST, DELETE, PUT

---
+++ GamesApi/Controllers/GamesController.cs
@@ -0,0 +1,62 @@
+using System.Net.Sockets;
+using GamesApi.Data;
+using GamesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GamesController : ControllerBase
+{
+   [HttpGet]
+   public ActionResult<List<Game>> GetAll()
+   {
+      return Ok(GamesStore.Games);
+   }
+
+
+   [HttpGet("{id}")]
+   public ActionResult<Game> GetById(int id)
+   {
+      var game = GamesStore.Games.FirstOrDefault(g => g.Id == id);
+      if (game is null)
+      {
+         return NotFound(new { message = $"Игра с id={id} не найдена" });
+      }
+      return Ok(game);
+   }
+
+   [HttpPost]
+   public ActionResult<Game> Create([FromBody] Game game)
+   {
+      game.Id = GamesStore.NextId();
+      GamesStore.Games.Add(game);
+      return CreatedAtAction(nameof(GetById), new { id = game.Id }, game);
+   }
+
+   [HttpDelete("{id}")]
+   public ActionResult Delete(int id)
+   {
+      var game = GamesStore.Games.FirstOrDefault(g => g.Id == id);
+      if (game is null)
+      {
+         return NotFound(new { message = $"Игра с id={id} не найдена" });
+      }
+      GamesStore.Games.Remove(game);
+      return NoContent();
+   }
+
+   [HttpPut("{id}")]
+   public ActionResult<Game> Update(int id, [FromBody] Game update)
+   {
+      var game = GamesStore.Games.FirstOrDefault(g => g.Id == id);
+      if (game is null){
+         return NotFound(new { message = $"Игра с id={id} не найдена" });
+      }
+      game.Title = update.Title;
+      game.Genre = update.Genre;
+      game.ReleaseYear = update.ReleaseYear;
+      return Ok(game);
+   }
+}
